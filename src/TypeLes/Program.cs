89633c42ec165d10a1590a30578ae88898e1ef73
Bart de Boer <bart.de.boer@divverence.com>
2019-02-01 19:20:13 +0100

    Fresh Project

---
+++ src/TypeLes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TypeLes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
