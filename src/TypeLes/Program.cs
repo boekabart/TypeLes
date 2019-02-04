25d273f0bdc3dc0a6cfe4a29600e1c801359c039
Bart de Boer <bart.de.boer@divverence.com>
2019-02-04 22:09:51 +0100

    Les1 Dag1

--- src/TypeLes/Program.cs
+++ src/TypeLes/Program.cs
@@ -39,8 +39,23 @@ namespace TypeLes
                 LesNr = 1,
                 DagNr = 1,
                 OefeningNr = 1,
-                Zin = "jfjf ffjj jfjj jjff"
+                Zin = @"ffjj fjff jfjf jjfj ffjj fjfj fjfj jfjf jfjf ffjj ffjj jfff ffjj jffj jjff ffjf jfjf jjfj ffjf jffj ffjj fjfj jfff ffjj
+ffjf jjjf ffjf jfjf jjfj ffjf jffj ffjj jfjf jfjf jjff ffjj jfjf ffjf jjjf fjjf jffj jfjf jfjf jjff jjff jfjf jfjf jfjf
+jjff ffjj jfjf jfjf fjff jjfj"
             },
+            new Oefening
+            {
+                LesNr = 1,
+                DagNr = 1,
+                OefeningNr = 2,
+                Zin = @"kkdk ddkd kkdd kkdd kdkd dkdk kkdk kkdd dkdk ddkk dkkd kddk dkdk kkdd kddk
+kkdd kkdk kkdd kdkd ddkk ddkd ddkk kkdk kkdd kdkd kddk
+kdkd kddd kdkk kddk ddkk kkdk ddkk dkkd ddkk kkdk kddk kdkd kddk
+kkdk ddkd kdkk ddkd kdkd kkdd kdkd kkkd kkdd kkdk kkdd kdkd ddkk
+kkdd kkdd kdkd dkdk kkdk ddkk kkdk kddk kkdk ddkk dkkd ddkk kdkd
+"
+            },
+/*
             new Oefening
             {
                 LesNr = 2, DagNr = 1, OefeningNr = 1,
@@ -49,7 +64,7 @@ namespace TypeLes
 rfuj rfuj rfrj ujuf rfuj ffjr fjuj fjru rfuj fjru jjuu frrf juuj rfuj frfj jujr frju
 ujfr rfuj rfuj fjrf jfrj ujfr rfju rfju rfuj juuj frrf ujju rfuj rfuj fjru jjuu frrf juuj
 rfrj ujuf rfuj fjrf fjuf"
-            }
+            }*/
         };
 
         private static readonly string[] Leerlingen = {"Sebbe", "Sophie", "Papa", "Mama"};
